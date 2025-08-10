163465b1e148dbe916c4ae9c8e63dcff2fad6ce0
Renato Alves <renatoalves19112008@gmail.com>
2025-08-10 14:57:08 +0000

    continue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased')} suppressHydrationWarning={true}>
-        {children}
+        <div className="animate-in fade-in-0 duration-500">
+            {children}
+        </div>
         <Toaster />
       </body>
     </html>
