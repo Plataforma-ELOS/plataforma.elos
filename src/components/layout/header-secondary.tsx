0a65731bf2153470531bd46a862d415273784c1c
Letícia Santana Lins <leticia.sanlins14@gmail.com>
2025-08-15 11:42:55 +0000

    adicione Perfis Profissionais no header de todas as páginas, atualmente

--- src/components/layout/header-secondary.tsx
+++ src/components/layout/header-secondary.tsx
@@ -16,6 +16,7 @@ const navItems = [
   { name: 'Acervo Digital', href: '/acervo-digital', isFeature: false },
   { name: 'Comunidade', href: '/comunidade', isFeature: false },
   { name: 'Suporte IA', href: '/suporte-ia', isFeature: false },
+  { name: 'Perfis Profissionais', href: '#', isFeature: true },
 ];
 
 export default function HeaderSecondary() {
