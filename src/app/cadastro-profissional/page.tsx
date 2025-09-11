3d8a74fb00fdc0a39656cff147c5ca5ed829d565
Letícia Santana Lins <leticia.sanlins14@gmail.com>
2025-09-11 10:20:40 +0000

    use este url para adicionar a logo no placeholder em frente ao nome Plat

--- src/app/cadastro-profissional/page.tsx
+++ src/app/cadastro-profissional/page.tsx
@@ -106,7 +106,7 @@ export default function ProfessionalSignUpPage() {
             <div className="mx-auto grid w-full max-w-md gap-8 mt-16 lg:mt-0">
                 <div className="grid gap-4 text-center">
                     <Link href="/home" className="flex items-center justify-center gap-2 text-2xl font-bold">
-                        <Image src="https://i.ibb.co/p3wFy56/icon-1-removebg-preview.png" width={40} height={40} alt="Logo" className="rounded-full" />
+                        <Image src="https://i.ibb.co/GQytCWPj/ELOS-Projeto-Integrador-PI.jpg" width={40} height={40} alt="Logo" className="rounded-full" />
                         <span className="text-foreground">Plataforma</span>
                         <span className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">E.L.O.S</span>
                     </Link>
