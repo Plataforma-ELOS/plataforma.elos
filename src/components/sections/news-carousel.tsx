7c94cbb43359a3f97b3000c7492a8c94d9d95c5a
Renato Alves <renatoalves19112008@gmail.com>
2025-09-12 11:06:55 +0000

    noa mudou nada

--- src/components/sections/news-carousel.tsx
+++ src/components/sections/news-carousel.tsx
@@ -65,7 +65,7 @@ const newsArticles = [
 
 export default function NewsCarousel() {
   return (
-    <section className="w-full py-20 md:py-24 bg-primary/10 dark:bg-primary/20 overflow-hidden relative">
+    <section className="w-full py-20 md:py-24 bg-primary/10 dark:bg-primary/20 overflow-hidden">
       <div className="container mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline text-center mb-16">
           Últimas notícias
@@ -105,13 +105,14 @@ export default function NewsCarousel() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="absolute left-0 sm:left-4 top-1/2 -translate-y-1/2 z-10 pointer-events-auto bg-background/50 border-none text-foreground hover:bg-background/80 hover:text-foreground" />
-          <CarouselNext className="absolute right-0 sm:right-4 top-1/2 -translate-y-1/2 z-10 pointer-events-auto bg-background/50 border-none text-foreground hover:bg-background/80 hover:text-foreground" />
+           <div className="absolute inset-y-0 inset-x-0 flex items-center justify-between pointer-events-none">
+                <div className="h-full w-32 bg-gradient-to-r from-primary/10 dark:from-primary/20 to-transparent z-10"></div>
+                <div className="h-full w-32 bg-gradient-to-l from-primary/10 dark:from-primary/20 to-transparent z-10"></div>
+            </div>
+          <CarouselPrevious className="absolute left-0 sm:left-4 top-1/2 -translate-y-1/2 z-20 pointer-events-auto bg-background/50 border-none text-foreground hover:bg-background/80 hover:text-foreground" />
+          <CarouselNext className="absolute right-0 sm:right-4 top-1/2 -translate-y-1/2 z-20 pointer-events-auto bg-background/50 border-none text-foreground hover:bg-background/80 hover:text-foreground" />
         </Carousel>
       </div>
-
-      <div className="absolute inset-y-0 left-0 w-32 bg-gradient-to-r from-primary/10 dark:from-primary/20 to-transparent z-10 pointer-events-none" />
-      <div className="absolute inset-y-0 right-0 w-32 bg-gradient-to-l from-primary/10 dark:from-primary/20 to-transparent z-10 pointer-events-none" />
     </section>
   );
 }
